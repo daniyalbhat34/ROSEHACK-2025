f01d6de59f1196b2e6d431456f72b5110c74187c
root <root@DESKTOP-3T2VE7C.>
2025-01-25 19:46:45 -0800

    Input checks

--- main.cpp
+++ main.cpp
@@ -3,14 +3,18 @@
 #include "character.h"
 #include "player.h"
 #include "weapons.h"
+#include <limits>
+#include <stdexcept>
 
 using namespace std;
 
 int main() {
-
     string characterName = "";
     cout << "Hello there adventurer! Please select your character name:" << endl;
-    getline(cin, characterName); 
+    getline(cin, characterName);
+    if(cin.fail()) {
+        throw runtime_error("Input stream operation failed \n");
+    }
     cout << "Welcome to the battle arena, "  << characterName << "! Are you ready to face off against a Level 1 Enemy?" << endl;
 
     Weapon weapon1(20);
@@ -18,14 +22,17 @@ int main() {
     Player player(weapon1);
 
     int num_fights = 0;
-
-
-    cout << "How many enemies would you like to fight?\n";
     bool not_valid = true;
 
     while (not_valid) {
+        cout << "How many enemies would you like to fight?\n";
         cin >> num_fights;
-        if (num_fights <= 0) {
+        if(cin.fail()) {
+            cout << "Input operation failed, try again" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        else if (num_fights <= 0) {
             cout << "Please enter a valid number\n";
         } else {
             not_valid = false;
--- player.cpp
+++ player.cpp
@@ -5,11 +5,12 @@ using namespace std;
 void Player::attack(Character& enemy) {
     enemy.health -= enemy.weapon.damage;
     enemy.stamina -= 5;
-    cout << "The damage was " << enemy.weapon.damage << "and the remaining enemy health is "
-        << enemy.health << endl;;
+    cout << "The damage was " << enemy.weapon.damage << " and your remaining enemy health is "
+        << enemy.health << endl;
 }
 void Player::heal() {
     health = min(100, health + stamina);
+    cout << "After healing, your health is " << health << endl;
 }
 
 
