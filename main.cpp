a1ab1ae95d93e854d2128c38b51bd41d512c18a9
root <root@DESKTOP-3T2VE7C.>
2025-01-25 19:13:37 -0800

    Edited main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@ using namespace std;
 #include <iostream>
 #include "fight.h"
 #include "character.h"
+#include "player.h"
 
-
-int main(){
+int main() {
 
     string characterName = "";
     cout << "Hello there adventurer! Please select your character name:" << endl;
     getline(cin, characterName); 
     cout << "Welcome to the battle arena, "  << characterName << "! Are you ready to face off against a Level 1 Enemy?" << endl;
+
+    Weapon weapon1(20);
+    Weapon weapon2(10);
+    Player player(weapon1);
+    Player enemy(weapon2);
+    start_fight(weapon,1, weapon2);
+}
